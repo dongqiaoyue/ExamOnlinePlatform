51b88aee1f0b9c0cfa76f4374adbe51cc049553c
hikeny <511983305@qq.com>
2020-11-28 15:07:43 +0800

    .

--- Exam_System_Loop/Code_Admin/config/db.php
+++ Exam_System_Loop/Code_Admin/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=stu_system',
-//    'dsn' => 'mysql:host=172.18.0.1;dbname=stu_system',
+//    'dsn' => 'mysql:host=localhost;dbname=stu_system',
+    'dsn' => 'mysql:host=172.18.0.1;dbname=stu_system',
     'username' => 'root',
-    'password' => 'root',
+    'password' => 'Cuitloop@123456',
     'charset' => 'utf8',
 ];
