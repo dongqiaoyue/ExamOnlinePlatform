b484c2a15f193d056f992a943035c552628064dd
bama404 <2822648066@qq.com>
2021-01-15 00:42:27 +0800

    .

--- Exam_System_Loop/Code_Admin/config/db.php
+++ Exam_System_Loop/Code_Admin/config/db.php
@@ -2,10 +2,10 @@
 
 return [
     'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=localhost;dbname=stu_system',
-    'dsn' => 'mysql:host=172.18.0.1;dbname=stu_system',
+    'dsn' => 'mysql:host=localhost;dbname=stu_system',
+  //  'dsn' => 'mysql:host=172.18.0.1;dbname=stu_system',
     'username' => 'root',
-    'password' => 'Cuitloop@123456',
+    'password' => '13508250749',
 //    'password' => '6226223019',
     'charset' => 'utf8',
 ];
